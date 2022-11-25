338930c907513e5ddf1d9ee5e8338158c4339966
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-11-25 17:56:43 +0000

    Naive Impl

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+pub type EditDistance = fn(&str, &str) -> usize;
+
+fn min(x: usize, y: usize, z: usize) -> usize {
+    std::cmp::min(x, std::cmp::min(y, z))
+}
+
+// https://en.wikipedia.org/wiki/Levenshtein_distance#Recursive
+pub fn naive(x: &str, y: &str) -> usize {
+    let Some(x1) = x.chars().next() else { return y.len() };
+    let Some(y1) = y.chars().next() else { return x.len() };
+
+    let x_rest = &x[x1.len_utf8()..];
+    let y_rest = &y[y1.len_utf8()..];
+
+    if x1 == y1 {
+        naive(x_rest, y_rest)
+    } else {
+        let d_insert = naive(x, y_rest);
+        let d_delete = naive(x_rest, y);
+        let d_swap = naive(x_rest, y_rest);
+        1 + min(d_insert, d_delete, d_swap)
+    }
+}
